100f0c4c38dc0da6bbee5a15d7f3d56a448984c6
Tyoma <risendrums777@gmail.com>
2022-01-04 18:39:00 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 
+import user from './data/user.json';
 import Container from './components/Container/Container';
 import Profile from './components/Profile/Profile';
 import Statistics from './components/Statistics/Statistics';
@@ -13,7 +14,13 @@ import transactions from './data/transactions.json';
 const App = () => {
   return (
     <Container>
-      <Profile />
+      <Profile
+        name={user.name}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
       <Statistics title="Upload stats" stats={statisticalData} />
       <FriendList friends={friends} />
       <TransactionHistory items={transactions} />
