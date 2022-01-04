760fdb1fa475baff5ddef187ea279a9f683c8229
Tyoma <risendrums777@gmail.com>
2022-01-04 18:39:02 +0200

    Update Profile.jsx

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -3,20 +3,13 @@ import React from 'react';
 import Description from './Description/Description';
 import StatisticsList from './StatisticsList/StatisticsList';
 
-import user from '../../data/user.json';
-
 import styles from './Profile.module.css';
 
-const Profile = () => {
+const Profile = ({ name, tag, location, avatar, stats }) => {
   return (
     <div className={styles.profile}>
-      <Description
-        name={user.name}
-        tag={user.tag}
-        location={user.location}
-        avatar={user.avatar}
-      />
-      <StatisticsList stats={user.stats} />
+      <Description name={name} tag={tag} location={location} avatar={avatar} />
+      <StatisticsList stats={stats} />
     </div>
   );
 };
